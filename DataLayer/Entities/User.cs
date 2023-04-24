7b77af79c5b582e5dbebe66d76bbcc117fdd44e5
Alex-dev02 <ionut02alex@yahoo.ro>
2023-04-24 14:12:18 +0300

    restructured Student and User entities to make more sense

--- DataLayer/Entities/User.cs
+++ DataLayer/Entities/User.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public UserRole Role { get; set; }
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+        public string Address { get; set; }
+
         public string PasswordHash { get; set; }
 
     }
